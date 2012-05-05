9cff2351fde4718eeec9a19648a318033d5e2d40
Rasmus Pedersen <rasmus@akvaservice.dk>
2012-05-06 00:18:29 +0200

    -

---
+++ Source/sConsole/javascript/runtime.js
@@ -0,0 +1,6 @@
+getMenuXML : function ()
+{
+	var request = new SNDK.ajax.request ("/", "cmd=Ajax;cmd.function=sConsole.runtime.getMenuXML", "data", "POST", false);		
+	request.send ();
+	return request.respons ()["menuxml"];
+}
