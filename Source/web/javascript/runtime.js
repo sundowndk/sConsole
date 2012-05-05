b423e899ff3c3dd45e25f3fe916644a09c07577e
Rasmus Pedersen <rasmus@akvaservice.dk>
2012-05-06 00:20:57 +0200

    -

--- Source/web/javascript/runtime.js
+++
@@ -1,6 +0,0 @@
-getMenuXML : function ()
-{
-	var request = new SNDK.ajax.request ("/", "cmd=Ajax;cmd.function=sConsole.runtime.getMenuXML", "data", "POST", false);		
-	request.send ();
-	return request.respons ()["menuxml"];
-}
--- Source/web/javascript/snapin.js
+++
@@ -1,6 +0,0 @@
-init : function (attributes)
-{
-	SNDK.includeJS ({url: UI.snapIn.url + attributes.name +"/include.js"});	
-	UI.snapIn[attributes.name].elements = SNDK.SUI.builder.construct ({URL: UI.snapIn.url + attributes.name +"/ui.xml", appendTo: attributes.appendTo});
-	UI.snapIn[attributes.name].init (attributes);
-}
