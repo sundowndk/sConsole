7284efe077701d54402311a6d68e735a42035b62
Rasmus Pedersen <rasmus@akvaservice.dk>
2012-05-10 07:52:30 +0200

    -

--- Source/sConsole/javascript/modal/window.js
+++ Source/sConsole/javascript/modal/window.js
@@ -82,6 +82,7 @@ window : function (_attributes)
 		sConsole.modal.depth++;
 		_initialized = true;
 		
+		
 		if (_attributes.XML)
 		{
 			_elements["ui"] = SNDK.SUI.builder.construct ({XML: _attributes.XML, appendTo: _elements["content"] });			
@@ -90,7 +91,6 @@ window : function (_attributes)
 		if (_attributes.SUIXML != null)
 		{
 			_elements["ui"] = SNDK.SUI.builder.construct ({URL: _attributes.SUIXML, appendTo: _elements["content"] });	
-
 		}
 		
 		SNDK.SUI.init ();		
